962cdba795520058248d75231613e5da67f0f0ab
David Sánchez <davidbrio@hotmail.com>
2022-04-29 20:09:29 +0200

    add viewBinding

--- app/src/main/java/com/isspass/myapplication/ui/MainActivity.kt
+++ app/src/main/java/com/isspass/myapplication/ui/MainActivity.kt
@@ -1,24 +1,24 @@
 package com.isspass.myapplication.ui
 
 import android.os.Bundle
-import android.widget.TextView
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
-import com.isspass.myapplication.R
+import com.isspass.myapplication.databinding.ActivityMainBinding
 import com.isspass.myapplication.viewmodel.MainViewModel
 
 class MainActivity : AppCompatActivity() {
 
     private val viewModel by viewModels<MainViewModel>()
+    private lateinit var viewBinding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
-        val message = findViewById<TextView>(R.id.message)
+        viewBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(viewBinding.root)
 
         viewModel.testMessage.observe(this){ value ->
-            message.text = value
+            viewBinding.message.text = value
         }
 
     }
